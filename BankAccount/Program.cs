453260a2ebdf76bba64ed488f071d630c61fe8b6
JonathanJohansson24 <Jonathan.nordin24@gmail.com>
2023-09-20 11:55:00 +0200

    gjorde en metod till AddSaldo

--- BankAccount/Program.cs
+++ BankAccount/Program.cs
@@ -4,6 +4,8 @@
     {
         static void Main(string[] args)
         {
+            Program withdraw = new Program();
+            
             bool myBool = true;
             int initialSaldo = 0;
 
@@ -25,20 +27,18 @@
                     {
                         case 1:
 
-                            Console.WriteLine("Du har valt att sätta in pengar på ditt konto!");
-                            Console.Write("Hur mycket vill du sätta in?: ");
-                            int addedSaldo = Convert.ToInt32(Console.ReadLine());
-                            initialSaldo += addedSaldo;
-                            Console.WriteLine("Du har valt att sätta in {0}kr på ditt konto", addedSaldo);
-                            Console.ReadKey();
+                            initialSaldo = AddSaldo(initialSaldo);
+                            
                             break;
                         case 2:
+
+
                             Console.WriteLine("Du har valt att ta ut pengar på från konto!");
                             Console.Write("Hur mycket vill du ta ut?: ");
                             int withdrawnSaldo = Convert.ToInt32(Console.ReadLine());
                             Console.ReadKey();
 
-                            if (withdrawnSaldo >= initialSaldo)
+                            if (withdrawnSaldo > initialSaldo)
                             {
                                 Console.WriteLine("Sorry, du kan inte ta ut mer än vad du har på kontot");
                                 Console.ReadKey();
@@ -49,7 +49,7 @@
                                 Console.WriteLine($"Du har valt att ta ut {withdrawnSaldo}kr, ditt saldo är just nu {initialSaldo}");
                                 Console.ReadKey();
                             }
-                            //newSaldo = withdrawnSaldo - saldo;
+
                             break;
                         case 3:
                             Console.WriteLine("Ditt nuvarande saldo är {0}", initialSaldo);
@@ -59,7 +59,6 @@
                             Console.WriteLine("Du har valt att lämna kontohanteringen, ha en bra dag!");
                             Console.ReadKey();
                             Environment.Exit(0);
-
                             break;
                         default:
                             break;
@@ -72,6 +71,20 @@
                 }
             }
         }
+        public static int AddSaldo(int initialSaldo)
+        {
+            Console.WriteLine("Du har valt att sätta in pengar på ditt konto!");
+            Console.Write("Hur mycket vill du sätta in?: ");
+            int addedSaldo = Convert.ToInt32(Console.ReadLine());
+            int sum = initialSaldo += addedSaldo;
+            Console.WriteLine("Du har valt att sätta in {0}kr på ditt konto", addedSaldo);
+            Console.ReadKey();
+            return sum;
+
+            
+        }
+
+        
 
 
     }
